f651b25e293f5c712e6a637fa39aeb481f7dfcbf
wangkewei1993 <1067647932@qq.com>
2021-12-20 17:06:11 +0800

    添加倾斜摄影

--- src/components/CesiumApp/some/Part.js
+++ src/components/CesiumApp/some/Part.js
@@ -25,6 +25,50 @@ export default class Part {
         })
     }
 
+    /**
+     * 加载bilbord
+     */
+    addIcon () {
+        this.app.viewer.entities.add({
+            position: Cesium.Cartesian3.fromDegrees(102.65425451755321, 24.902048298657025, 1856.639104950962),
+            // 点
+            point: {
+                color: Cesium.Color.RED, // 点位颜色
+                pixelSize: 10 // 像素点大小
+            },
+            // 文字
+            label: {
+                // 文本。支持显式换行符“ \ n”
+                text: '1号房',
+                // 字体样式,以CSS语法指定字体
+                font: '14pt Source Han Sans CN',
+                // 字体颜色
+                fillColor: Cesium.Color.BLACK,
+                // 背景颜色
+                backgroundColor: Cesium.Color.AQUA,
+                // 是否显示背景颜色
+                showBackground: true,
+                // 字体边框
+                outline: true,
+                // 字体边框颜色
+                outlineColor: Cesium.Color.WHITE,
+                // 字体边框尺寸
+                outlineWidth: 10,
+                // 应用于图像的统一比例。比例大于会1.0放大标签,而比例小于会1.0缩小标签。
+                scale: 1.0,
+                // 设置样式：FILL：填写标签的文本,但不要勾勒轮廓；OUTLINE：概述标签的文本,但不要填写；FILL_AND_OUTLINE：填写并概述标签文本。
+                style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+                // 相对于坐标的水平位置
+                verticalOrigin: Cesium.VerticalOrigin.CENTER,
+                // 相对于坐标的水平位置
+                horizontalOrigin: Cesium.HorizontalOrigin.LEFT,
+                // 该属性指定标签在屏幕空间中距此标签原点的像素偏移量
+                pixelOffset: new Cesium.Cartesian2(10, 0),
+                // 是否显示
+                show: true
+            }
+        })
+    }
 
     /**
      * 加载流动墙效果
@@ -120,51 +164,6 @@ export default class Part {
         this.app.viewer.entities.add(flowWall)
 
         this.app.viewer.zoomTo(this.app.viewer.entities)
-    }
-
-    /**
-     * 加载bilbord
-     */
-    addIcon () {
-        this.app.viewer.entities.add({
-            position: Cesium.Cartesian3.fromDegrees(102.65425451755321, 24.902048298657025, 1856.639104950962),
-            // 点
-            point: {
-                color: Cesium.Color.RED, // 点位颜色
-                pixelSize: 10 // 像素点大小
-            },
-            // 文字
-            label: {
-                // 文本。支持显式换行符“ \ n”
-                text: '1号房',
-                // 字体样式,以CSS语法指定字体
-                font: '14pt Source Han Sans CN',
-                // 字体颜色
-                fillColor: Cesium.Color.BLACK,
-                // 背景颜色
-                backgroundColor: Cesium.Color.AQUA,
-                // 是否显示背景颜色
-                showBackground: true,
-                // 字体边框
-                outline: true,
-                // 字体边框颜色
-                outlineColor: Cesium.Color.WHITE,
-                // 字体边框尺寸
-                outlineWidth: 10,
-                // 应用于图像的统一比例。比例大于会1.0放大标签,而比例小于会1.0缩小标签。
-                scale: 1.0,
-                // 设置样式：FILL：填写标签的文本,但不要勾勒轮廓；OUTLINE：概述标签的文本,但不要填写；FILL_AND_OUTLINE：填写并概述标签文本。
-                style: Cesium.LabelStyle.FILL_AND_OUTLINE,
-                // 相对于坐标的水平位置
-                verticalOrigin: Cesium.VerticalOrigin.CENTER,
-                // 相对于坐标的水平位置
-                horizontalOrigin: Cesium.HorizontalOrigin.LEFT,
-                // 该属性指定标签在屏幕空间中距此标签原点的像素偏移量
-                pixelOffset: new Cesium.Cartesian2(10, 0),
-                // 是否显示
-                show: true
-            }
-        })
     }
 
     /**
