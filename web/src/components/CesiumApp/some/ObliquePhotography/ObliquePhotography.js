666afafc5be506354d9e4df6bf40029221b1c792
wangkewei <1067647932@qq.com>
2022-06-29 10:09:52 +0800

    升级cesium版本

--- web/src/components/CesiumApp/some/ObliquePhotography/ObliquePhotography.js
+++ web/src/components/CesiumApp/some/ObliquePhotography/ObliquePhotography.js
@@ -1,5 +1,4 @@
-import * as Cesium from 'cesium/Cesium'
-import * as widget from 'cesium/Widgets/widgets.css'
+
 import {ImageryProviderWebExtendTool} from "./1-2ImageryProvider-WebExtend"
 
 export default class ObliquePhotography {
